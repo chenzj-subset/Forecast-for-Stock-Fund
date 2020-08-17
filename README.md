57ea8831fc74da7e2c75353ddb23adcf93626e1c
chenzj-subset <1419908594@qq.com>
2020-08-17 20:54:07 +0800

    get data from tushare

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Forecast-for-Stock-Fund
 Use market factors and historical data to predict the future of stocks and funds
+Step 0ne:
+    get the stock or fund information from Tushare
+Step Two:
+    use LSTM
---
+++ Step_one.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import tushare as ts
+
+ts.set_token('cd386d3761611059bea60abb186636554725860c63da71637b466d44')
+# my token here
+# you can get your own token from https://tushare.pro/register?reg=387255
+pro = ts.pro_api()
+# initialize the api
+listall = pro.stock_basic(exchange='', list_status='L',
+                          fields='ts_code,symbol,name,area,industry,list_date')
+# stock list
+# print(listall.columns)
+# Index(['ts_code', 'symbol', 'name', 'area', 'industry', 'list_date'], dtype='object')
+ts_code_list = list(set(listall.ts_code[0:99]))
+for ts_code in ts_code_list:
+    k = pro.daily(ts_code=ts_code,
+                  start_date='20200101', end_date='20200817')
+    # K lines
+    print(k)
